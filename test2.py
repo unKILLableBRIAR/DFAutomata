229e13931f22d2e8cc43cd7cb4fde24c0c4810da
unKILLableBRIAR <wnsgud3590@naver.com>
2024-10-14 14:47:53 +0900

    test2파일에 csv를 dfa로 변경시킬 수 있도록 완성했습니다.

--- test2.py
+++ test2.py
@@ -1,19 +1,39 @@
 import csv
+import DFAutomata as dfa
 
 f = open('test.csv', 'r')
 ls = []
-tmp_dict = {}
+dfa_map = {}
 rdr = csv.reader(f)
 
 start_state = '-'
 final_state = '*'
 
-tmp_str = next(rdr)
+#리스트에서 딕셔너리의 키값 추출
+key_str = next(rdr)
+dfa.frontPop(key_str)
+
+for item in key_str:
+    dfa_map[item] = {}
 
 for item in rdr:
-    print(item)
+    tmp_key = dfa.frontPop(item)
+
+    if start_state == '-':
+        if '-' in tmp_key:
+            tmp_key = tmp_key[0]
+            start_state = tmp_key
+    elif final_state == '*':
+        if '*' in tmp_key:
+            tmp_key = tmp_key[0]
+            final_state = tmp_key
+
+    for item2 in key_str:
+        dfa_map[item2][tmp_key] = dfa.frontPop(item)
 
 print(start_state)
 print(final_state)
+
+print(dfa_map)
 
 f.close()
